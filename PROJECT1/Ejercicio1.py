7947ecbe56ff27b88ea8a8cb186a68abcf78d9a3
5starcannon <joan.puig.sds22@gmail.com>
2022-10-04 19:35:47 +0200

    fff
    ff

--- PROJECT1/Ejercicio1.py
+++ PROJECT1/Ejercicio1.py
@@ -10,29 +10,45 @@ t.forward(100)
 t.right(90)
 t.forward(100)
 
-def funcion1square(x):
+
+
+def funcion1square():
         for _ in range(4):
                 t.fd(x)
                 t.rt(90)
 for _ in range(10):
         funcion1square(100)
         t.rt(360/10)
-        
-def star(l, n):
+    
+def funcion1filledSquare():
+    print('Has elegido el Ejercicio 1 Filled Square')
+    
+def funcion1multiSquare():
+    print('Has elegido el Ejercicio 1 MultiSquare')
+  
+def funcion1polygon(l, n):
+   for i in range(7):
+      t.forward(l) 
+      t.right(n)
+funcion1polygon(100, 51.6)
+
+def funcion1circle():
+    print('Has elegido el Ejercicio 1 Circle')
+    
+def funcion1filledCircle():
+    print('Has elegido el Ejercicio 1 Filled Circle')
+    
+def funcion1squareCircles():
+    print('Has elegido el Ejercicio 1 Square Circles')
+    
+def funcion1star(l, n):
         for _ in range(n):
                 t.fd(l)
                 t.bk(l)
                 t.rt(360/n)
-star(100, 20)
+        funcion1star(100, 20)
 
-def polygon(l, n):
-   for i in range(7):
-      t.forward(l) 
-      t.right(n)
-polygon(100, 51.6)
-
-
-def star(x, y, l, n):
+def funcion1stars(x, y, l, n):
     t.penup()
     t.home()
     t.fd(x)
@@ -45,7 +61,14 @@ def star(x, y, l, n):
             t.rt(360/n)
 for _ in range(20):
     t.speed(500)
-    star(random.randint(-300, 300),
+    funcion1stars(random.randint(-300, 300),
          random.randint(-300, 300),
          random.randint(10, 150),
          random.randint(3, 30))
+
+    
+def funcion1AgraphPlotter():
+    print('Has elegido el Ejercicio 1A Graph Plotter')
+    
+def funcion1Bclock():
+    print('Has elegido el Ejercicio 1A Clock')
--- PROJECT1/FuncionesMenu.py
+++ PROJECT1/FuncionesMenu.py
@@ -65,38 +65,6 @@ def submenuEjercicio1():
 
     generate_menu(opciones, 'b')
 
-def funcion1square():
-    print('Has elegido el Ejercicio 1 Square')
-    
-def funcion1filledSquare():
-    print('Has elegido el Ejercicio 1 Filled Square')
-    
-def funcion1multiSquare():
-    print('Has elegido el Ejercicio 1 MultiSquare')
-
-def funcion1polygon():
-    print('Has elegido el Ejercicio 1 Polygon')
-    
-def funcion1circle():
-    print('Has elegido el Ejercicio 1 Circle')
-    
-def funcion1filledCircle():
-    print('Has elegido el Ejercicio 1 Filled Circle')
-    
-def funcion1squareCircles():
-    print('Has elegido el Ejercicio 1 Square Circles')
-    
-def funcion1star():
-    print('Has elegido el Ejercicio 1 Star')
-    
-def funcion1stars():
-    print('Has elegido el Ejercicio 1 Stars')
-    
-def funcion1AgraphPlotter():
-    print('Has elegido el Ejercicio 1A Graph Plotter')
-    
-def funcion1Bclock():
-    print('Has elegido el Ejercicio 1A Clock')
 
 #Funciones Submenús Ejercicio 2
 
