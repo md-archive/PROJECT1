3a242f1b7b157db7f8fd67cdd1fb6d0cb33885cd
RyusheiYT <garfield.025@hotmail.com>
2022-11-01 17:04:02 +0100

    Modified

--- PROJECT1/Ejercicio3.py
+++ PROJECT1/Ejercicio3.py
@@ -23,31 +23,42 @@ d1 = [0, 4, 8]
 d2 = [2, 4, 6]
 lines = [h1, h2, h3, v1, v2, v3, d1, d2]
 NumeroRandom = random.randint(0, 8)
-intersecting_lines = [(h1, v1, 0), (h1, v2, 1), (h1, v3, 2), (h2, v1, 3), (h2, v2, 4), (h2, v3, 5), (h3, v1, 6), (h3, v2, 7), (h3, v3, 8), (d1, h1, 0), (d1, h2, 4), (d1, h3, 8), (d1, v1, 0), (d1, v2, 4), (d1, v3, 8), (d2, h1, 2), (d2, h2, 4), (d2, h3, 6), (d2, v1, 2), (d2, v2, 4), (d2, v3, 6), (d1, d2, 4)]
-pl ='0'
-pl2 ='X'
+
+intersecting_lines = [(h1, v1, 0), (h1, v2, 1), (h1, v3, 2), (h2, v1, 3),
+                      (h2, v2, 4), (h2, v3, 5), (h3, v1, 6), (h3, v2, 7),
+                      (h3, v3, 8), (d1, h1, 0), (d1, h2, 4), (d1, h3, 8),
+                      (d1, v1, 0), (d1, v2, 4), (d1, v3, 8), (d2, h1, 2),
+                      (d2, h2, 4), (d2, h3, 6), (d2, v1, 2), (d2, v2, 4),
+                      (d2, v3, 6), (d1, d2, 4)]
+pl = '0'
+pl2 = 'X'
+
+
 def print_board(b):
     for n, x in enumerate(b):
         print(x, end='')
-        if n == 2: 
+        if n == 2:
             print('  012')
         elif n == 5:
             print('  345')
         elif n == 8:
             print('  678\n')
 
+
 def instructions():
     print('Board is numbered\n012\n345\n678')
-    
+
+
 def full(b):
     return '_' not in b
 
 
 def wins(p, b):
     win = [p, p, p]
-    for l in lines:
-        bl = [b[x] for x in l]
-        if bl == win: return True
+    for ll in lines:
+        bl = [b[x] for x in ll]
+        if bl == win:
+            return True
     return False
 
 
@@ -59,17 +70,21 @@ def random_play(pl, b):
     b[p] = pl
 
 # Dos Maquinas
+
+
 def random_game():
     b = emptyboard.copy()
-    pl ='0'
+    pl = '0'
     while not (full(b) or wins('X', b) or wins('0', b)):
         print_board(b)
-        print("\n--------------------") 
+        print("\n--------------------")
         random_play(pl, b)
-        if pl == '0': pl ='X'
-        else: pl = '0'
+        if pl == '0':
+            pl = 'X'
+        else:
+            pl = '0'
     print_board(b)
-    print("\n--------------------") 
+    print("\n--------------------")
     print('Game over. Result:')
     if wins('0', b):
         print('0 wins!')
@@ -79,15 +94,17 @@ def random_game():
         print('Any!')
 
 # Una Maquina
+
+
 def random_game_1():
     b = emptyboard.copy()
-    pl ='X'
+    pl = 'X'
     while not (full(b) or wins('X', b)):
         print_board(b)
-        print("\n--------------------") 
+        print("\n--------------------")
         random_play(pl, b)
     print_board(b)
-    print("\n--------------------") 
+    print("\n--------------------")
     print('Game over. Result:')
     if wins('X', b):
         print('X wins!')
@@ -97,6 +114,8 @@ def random_game_1():
 # Question 4
 
 # Question 5
+
+
 def try_to_take(b, ps):
     for p in ps:
         if b[p] == '_':
@@ -104,23 +123,29 @@ def try_to_take(b, ps):
             return True
     return False
 
+
 def tactic_play_centre(b):
     return try_to_take(b, [4])
+
 
 def tactic_first_blank(b):
     return try_to_take(b, [0, 1, 2, 3, 4, 5, 6, 7, 8])
 
+
 def win_or_block(b, piece):
-    for l in lines:
-        bl = [b[x] for x in l]
+    for ll in lines:
+        bl = [b[x] for x in ll]
         if bl.count('_') == 1 and bl.count(piece) == 2:
-            for x in l:
-                if b[x] == '_': b[x] = 'X'
+            for x in ll:
+                if b[x] == '_':
+                    b[x] = 'X'
             return True
     return False
 
+
 def tactic_win(b):
     return win_or_block(b, 'X')
+
 
 def tactic_block(b):
     return win_or_block(b, '0')
@@ -128,9 +153,9 @@ def tactic_block(b):
 
 def human_move(pl, board):
     n_input = input('\nPosition 0..8? ')
-    #print(pl)
+    # print(pl)
     if n_input.isdigit():
-        n= int(n_input)
+        n = int(n_input)
         if n < 0 or n > 8:
             print('Board position must be from 0..8 ')
             human_move(pl, board)
@@ -146,21 +171,28 @@ def human_move(pl, board):
 
 # Question 6
 
+
 def tactic_empty_corner(b):
     return try_to_take(b, [0, 2, 6, 8])
+
 
 def tactic_empty_side(b):
     return try_to_take(b, [1, 3, 5, 7])
 
+
 def tactic_play_opposite_corner(b):
     if b[0] == 'X':
-        if try_to_take(b, [8]): return True
+        if try_to_take(b, [8]):
+            return True
     elif b[2] == 'X':
-        if try_to_take(b, [6]): return True
+        if try_to_take(b, [6]):
+            return True
     elif b[6] == 'X':
-        if try_to_take(b, [2]): return True
+        if try_to_take(b, [2]):
+            return True
     elif b[8] == 'X':
         return try_to_take(b, 0)
+
 
 def computer_move(b):
     print('\nComputer has played:')
@@ -199,10 +231,10 @@ def play(human_goes_first, estonto):
         if human_goes_first:
             human_move(pl, board)
         else:
-            if(estonto):
-                random_play (pl2, board)
+            if (estonto):
+                random_play(pl2, board)
             else:
-                computer_move (board)
+                computer_move(board)
         human_goes_first = not human_goes_first
         print_board(board)
     print('\nGame over. Result:')
@@ -214,60 +246,60 @@ def play(human_goes_first, estonto):
         print('Draw!')
 
 
-##Maquina 2
-#random_game()
-##Maquina 1
-#random_game_1()
+# Maquina 2
+# random_game()
+# Maquina 1
+# random_game_1()
 
-## Dos perosnas
-#print('Board is numbered\n012\n345\n678\n')
+# Dos perosnas
+# print('Board is numbered\n012\n345\n678\n')
 
 
-#while not (full(b) or wins('X', b) or wins('0', b)):
+# while not (full(b) or wins('X', b) or wins('0', b)):
 #        print_board(b)
-#        print("\n--------------------") 
+#        print("\n--------------------")
 #        human_move(pl, b)
 #        if pl == '0': pl ='X'
 #        else: pl = '0'
-#print_board(b)
-#print("\n--------------------") 
-#print('Game over. Result:')
-#if wins('0', b):
+# print_board(b)
+# print("\n--------------------")
+# print('Game over. Result:')
+# if wins('0', b):
 #    print('0 wins!')
-#elif wins('X', b):
+# elif wins('X', b):
 #    print('X wins!')
-#else:
+# else:
 #    print('Any!')
 
 
-## Maquina y persona InicioAleatorio
-#quien = NULL
-#print("Random player")
+# Maquina y persona InicioAleatorio
+# quien = NULL
+# print("Random player")
 
-#first = random.randint(1,2)
+# first = random.randint(1,2)
 
-#if first == int(1) :
+# if first == int(1) :
 #   quien == True
-#else:
+# else:
 #    quien == False
-#play(quien)
+# play(quien)
 
 
-## Maquina y persona InicioEscogido
+# Maquina y persona InicioEscogido
 quienjuega = False
 estonto = False
 quien = NULL
 dificultad = NULL
 opciones = ('1', '2')
+
 while (quien not in opciones and dificultad not in opciones):
     quien = input('\nChoose Player PLAYER(1) or PC(2)? ')
     dificultad = input('\nChoose Machine TONTO(1) or LISTO(2)? ')
 
-if quien == '1' :
-   quienjuega = True
+if quien == '1':
+    quienjuega = True
 
-if dificultad == '1' :
-   estonto = True
+if dificultad == '1':
+    estonto = True
 
 play(quienjuega, estonto)
-
