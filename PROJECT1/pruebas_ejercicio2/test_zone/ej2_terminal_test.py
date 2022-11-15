82dd276c70eead71669a2b27a600ca71d6ef260d
5starcannon <joan.puig.sds22@gmail.com>
2022-11-15 19:54:52 +0100

    control de errores en los inputs

--- PROJECT1/pruebas_ejercicio2/test_zone/ej2_terminal_test.py
+++ PROJECT1/pruebas_ejercicio2/test_zone/ej2_terminal_test.py
@@ -2,23 +2,31 @@ import datetime
 import sys
 import os
 import csv
-
+import re
 def table_of_file(filename):
     # dirname = os.path.dirname(__file__)
     # filename = os.path.join(dirname, 'calories.txt')
     try:            # %%% Si el usuario introduce una fecha no valida %%%
         with open(filename) as row:
-            table = {}
+            table = {}  
             for cursor in row.readlines():
                 fields = cursor.split()
                 key = fields[0]
                 values = fields[1:]
                 table[key] = values
-                #print(f'{key[0:]} {values[0]} {values[1]} {values[2]} {values[3]}')
-                print(f'{key[0:]} {values[0]} {values[1]} {values[2]}')
+                    #print(f'{key[0:]} {values[0]} {values[1]} {values[2]} {values[3]}')
+                if len(values) == 3:
+                    print(f'{key[0:]} {values[0]} {values[1]} {values[2]}')
+                if len(values) == 4:
+                    print(f'{key[0:]} {values[0]} {values[1]} {values[2]} {values[3]}')
+            if len(table) == 0:
+                print("ERROR! File was found, but no data on it.")
+                #print("Add new food by typing your name, that food and its weight.\n")
+            else:
+                print("\n")
             return table
     except:
-        print("ERROR!! date from name not registered.")
+        print("ERROR!! File is not found. Have you already created?")
 
 # def table_of_file(filename):
 #     with open(filename) as c:
@@ -45,8 +53,14 @@ def list_weights(name):
 def list_dates(name):
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname,name)
-    for filename in sorted(os.listdir(filename)):
-        if filename != 'weight.txt': print(filename[:-3])
+    try:
+        sort_filename = os.listdir(filename)
+        for filename in sorted(sort_filename):
+            if filename != 'weight.txt': print(filename[:-3])
+        print("\n")
+    except:
+        print(f"{name}'s is not registered. Is this user created?")
+
 
 def list_foods():
     dirname = os.path.dirname(__file__)
@@ -68,7 +82,7 @@ def list_foods():
                     valores = valores.replace(ch,"")
             print(valores, end=' ')
         print('')
-
+    print("\n")
 def lookup_calories(food):
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, 'calories.txt')
@@ -85,6 +99,7 @@ def lookup_calories(food):
                             if ch in valores:
                                 valores = valores.replace(ch,"")
                         print(valores, end=' ')
+                        print(f'There are {valores[0]} calories in {weight[1]}g of {llaves}')
         # x = llaves.replace("{","")
         # y = llaves.replace("'","")
         # z = llaves.replace(":","")
@@ -101,7 +116,7 @@ def lookup_calories(food):
         if len(vs) > 0:
             weight = vs[0]
             calories = vs[0]
-            print(f'There are {calories} calories in {weight}g of {food}')
+            #print(f'There are {calories} calories in {weight}g of {food}')
         else:
             print(f'Malformed calorie entry for {food} in calories file')
 
@@ -157,7 +172,7 @@ def eaten(name, food, food_weight):
     is_new = not os.path.exists(filename)
     with open(os.path.join(filename, date_today()) + '.txt', 'a') as f:
         if is_new: print('Food,Weight', file=f)
-        print(f'Food selected : {food} \n Current food weight : {food_weight}g' , file=f)
+        print(f'Food selected : {food} \n Current food weightttttttt : {food_weight}g' , file=f)
 #
 
 def weighed(name, person_weight):
@@ -175,51 +190,116 @@ def date_today():
     return full_date
 
 # arg = sys.argv
-
-arg = input("Write Command and Descritpion: ").split()
+print("_______________________________________________________________________________")
+print("\n    <-- Command  -->                 <-- Description  -->  ")
+print("-------------------------------------------------------------------------------")
+print("list eaten <name> <date>      List foods eaten on a given date.")
+print("list weights <name>           List the weight history for a user.")
+print("list dates <name>             List the dates for which there are calorie counts.")
+print("list foods                    List all calorie data for known foods.")
+print("lookup calories <food>        Lookup up calories for a given food.")
+print("lookup weight <name> <date>   Lookup up weight for given date")
+print("total <name> <date>           Calculate total calories for a given user and date.")
+print("newuser <name>                Create a new user.")
+print("eaten <name> <food> <weight>  Add food data for today.")
+print("weighed <name> <weight>       Add weight data for today.\n")
+print("-------------------------------------------------------------------------------")
+print("Write your name in lower case [a-z]")
+print("Format date should be this one : '%d-%m-%Y'")
+print("-------------------------------------------------------------------------------\n")
+arg = input("Write Command and Description: ").split()
 
 if len(arg) > 0:
     cmd = arg[0]
-    if cmd == 'list':
-        if len(arg) > 2 and arg[1] == 'eaten':
-            list_eaten(arg[2], arg[3])
-        else:
-            if arg[1] == 'weights' and len(arg) > 2:
-                list_weights(arg[2])
-            elif arg[1] == 'dates' and len(arg) > 2:
-                list_dates(arg[2])
-            elif arg[1] == 'foods':
-                list_foods()
+    if len(arg) > 1:
+        if cmd == 'list':
+            if len(arg) > 2 and arg[1] == 'eaten':
+                if len(arg) == 4:
+                    if re.search("[a-z]$", arg[2]):
+                        try:
+                            date_format = datetime.datetime.strptime(arg[3],'%d-%m-%Y')
+                            print("\n-----------------------------------------------")
+                            print(f"Consulting {arg[2]}'s foods eaten on {arg[3]}.")
+                            print("-----------------------------------------------\n")
+                            list_eaten(arg[2], arg[3])
+                        except:
+                            print("ERROR! Mismatched format date, should be this one : '%d-%m-%Y'")
+                    else:
+                        print("ERROR! 3rd parameter must be a name")
+                else:
+                    print("ERROR! Not enough, to many or wrong parameters for listing foods from a user on a given day.")
+                    print("Correct format : list eaten <name> <date>")
+            else:
+                if arg[1] == 'weights' and len(arg) > 2:
+                    if len(arg) == 3:
+                        if re.search("[a-z]$", arg[2]):
+                            print("\n-----------------------------------------------")
+                            print(f"Consulting {arg[2]}'s weights.")
+                            print("-----------------------------------------------\n")
+                            list_weights(arg[2])
+                        else:
+                            print("ERROR! 3rd parameter must be a name")
+                    else:
+                        print("ERROR! Not enough, to many or wrong parameters for listing weight from a given user.")
+                        print("Correct format : list weights <name>")
+                elif arg[1] == 'dates' and len(arg) > 2:
+                    if len(arg) == 3:
+                        if re.search("[a-z]$", arg[2]):
+                            print("\n-----------------------------------------------")
+                            print(f"Consulting {arg[2]}'s dates from calorie registration.")
+                            print("-----------------------------------------------\n")
+                            list_dates(arg[2])
+                        else:
+                            print("ERROR! 3rd parameter must be a name")
+                    else:
+                        print("ERROR! Not enough, to many or wrong parameters for listing dates from a given user.")
+                        print("Correct format : list dates <name>")
+                elif arg[1] == 'foods':
+                    if len(arg) == 2:
+                        print("\n-----------------------------------------------")
+                        print(f"These are the foods available to register for your user.")
+                        print("-----------------------------------------------")
+                        list_foods()
+                    else:
+                        print("ERROR! Not enough or to many parameters for listing foods on a given day.")
+                else:
+                    print("Missing parameters. Are 'name' and 'date' both mentioned?")
+                    print("Correct format : list eaten <name> <date>")
+        elif cmd == 'lookup':
+            if len(arg) > 1:
+                if arg[1] == 'calories':
+                    if len(arg) == 3:
+                        lookup_calories(arg[2])
+                    elif len(arg) == 2:
+                        print("ERROR! Is the food mentioned?")
+                        print("Correct format : lookup calories <food>")
+                    else:
+                        print("ERROR! Not enough or to many parameters for listing foods on a given day.")
+                elif arg[1] == 'weight' and len(arg) > 2:
+                    lookup_weight(arg[2], arg[3])
+                else:
+                    print("Missing parameters. Are 'calories' and 'weight' both mentioned?")
+        elif cmd == 'total':
+            if len(arg) > 2:
+                total_date(arg[1], arg[2])
             else:
                 print("Missing parameters. Are 'name' and 'date' both mentioned?")
-            
-    elif cmd == 'lookup':
-        if len(arg) > 1:
-            if arg[1] == 'calories':
-                lookup_calories(arg[2])
-            elif arg[1] == 'weight' and len(arg) > 2:
-                lookup_weight(arg[2], arg[3])
+        elif cmd == 'newuser':
+            if len(arg) > 1:
+                new_user(arg[1])
             else:
-                print("Missing parameters. Are 'calories' and 'weight' both mentioned?")
-    elif cmd == 'total':
-        if len(arg) > 2:
-            total_date(arg[1], arg[2])
+                print("Missing parameter. Is the 'name' of the user mentioned?")
+        elif cmd == 'eaten':
+            if len(arg) > 3:
+                eaten(arg[1], arg[2], arg[3])
+            else:
+                print("Missing parameters. Are the 'name', 'food' and 'weight' all of them mentioned?")
+        elif cmd == 'weighed':
+            if len(arg) > 2:
+                weighed(arg[1], arg[2])
+            else:
+                print("Missing parameters. Are the 'name' and 'weight' both mentioned?")
         else:
-            print("Missing parameters. Are 'name' and 'date' both mentioned?")
-    elif cmd == 'newuser':
-        if len(arg) > 1:
-            new_user(arg[1])
-        else:
-            print("Missing parameter. Is the 'name' of the user mentioned?")
-    elif cmd == 'eaten':
-        if len(arg) > 3:
-            eaten(arg[1], arg[2], arg[3])
-        else:
-            print("Missing parameters. Are the 'name', 'food' and 'weight' all of them mentioned?")
-    elif cmd == 'weighed':
-        if len(arg) > 2:
-            weighed(arg[1], arg[2])
-        else:
-            print("Missing parameters. Are the 'name' and 'weight' both mentioned?")
+            print('Command not understood')
     else:
-        print('Command not understood')
+        print("ERROR!! At least 2 parameters are required.")
